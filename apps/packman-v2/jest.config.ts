c53dbc4cc3575eabac5bfc99826f777e9e86f54c
Nathalie.Sonilhac <robert_nathalie@hotmail.fr>
2025-03-03 12:00:53 +0100

    Initial commit

---
+++ apps/packman-v2/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'packman-v2',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/packman-v2',
+};
